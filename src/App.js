27a51a23485d341e93d800cd9674ff163bb65d01
kadunapikin <ibakoshi27@gmail.com>
2023-08-07 23:12:00 +0100

    import BrowserRouter from react-router-dom and define the Routes to the pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 
 function App() {
   return (
-    <div>App</div>
-  )
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/projects" element={<Projects />}></Route>
+          <Route path="/experience" element={<Experience />}></Route>
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
-export default App
+export default App;
