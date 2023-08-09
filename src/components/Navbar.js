73a10fe563beb838e55b7a60a2196dd5ae39d871
kadunapikin <ibakoshi27@gmail.com>
2023-08-09 15:43:50 +0100

    create a useState to setExpandNavbar to either open or close onClick

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import "../styles/Navbar.css";
 import ReorderIcon from '@mui/icons-material/Reorder';
 
 function Navbar() {
+  const [expandNavbar, setExpandNavbar] = useState(false);
+  const handleClick = () => {
+    setExpandNavbar((prev) => !prev);
+  }
   return (
-    <div className='navbar'>
+    <div className='navbar' id={expandNavbar ? "open" : "close"}>
         <div className='toggleButton'>
-            <button> <ReorderIcon /> </button>
+            <button onClick={handleClick}>
+              <ReorderIcon />
+            </button>
         </div>
         <div className='links'>
             <Link to="/">Home</Link>
