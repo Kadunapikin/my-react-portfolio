c09d9fbf58ab02a3f37ef11620272f8ecb0a97a9
kadunapikin <ibakoshi27@gmail.com>
2023-08-10 12:38:17 +0100

    Add work experience on Experience.js

--- src/pages/Experience.js
+++ src/pages/Experience.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
 import 'react-vertical-timeline-component/style.min.css';
 import SchoolIcon from '@mui/icons-material/School';
+import WorkIcon from '@mui/icons-material/Work';
 
 function Experience() {
   return (
@@ -29,6 +30,30 @@ function Experience() {
         icon={<SchoolIcon />}>
           <h3 className='vertical-timline-element-title'>TOP Developers, Nigeria</h3>
           <p>Front End Web Development (Remote)</p>
+        </VerticalTimelineElement>
+
+        <VerticalTimelineElement className='vertical-timeline-element--education' 
+        date='2015-2019' 
+        iconStyle={{ background: '#3e497a', color: '#fff' }}
+        icon={<WorkIcon />}>
+          <h3 className='vertical-timline-element-title'>Lift Above Poverty Organization (LAPO) Micro Finance Bank Nigeria Limited.</h3>
+          <p>Agricultural Client Support Officer</p>
+        </VerticalTimelineElement>
+
+        <VerticalTimelineElement className='vertical-timeline-element--education' 
+        date='20019-2022' 
+        iconStyle={{ background: '#3e497a', color: '#fff' }}
+        icon={<WorkIcon />}>
+          <h3 className='vertical-timline-element-title'>Letshego MFB, Abuja.</h3>
+          <p>Agricultural Team Lead.</p>
+        </VerticalTimelineElement>
+
+        <VerticalTimelineElement className='vertical-timeline-element--education' 
+        date='2022-Till Date' 
+        iconStyle={{ background: '#3e497a', color: '#fff' }}
+        icon={<WorkIcon />}>
+          <h3 className='vertical-timline-element-title'>TOP Software Developers,  Nigeria.</h3>
+          <p>Volunteer Mentorship (Remote)</p>
         </VerticalTimelineElement>
 
       </VerticalTimeline>
