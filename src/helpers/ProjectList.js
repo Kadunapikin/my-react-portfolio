154fcf3d0a4264bf5c195054a704ad930e558df1
kadunapikin <ibakoshi27@gmail.com>
2023-08-11 00:10:20 +0100

    create helpers dir and add ProjectList.js with project names and images as array of objects

---
+++ src/helpers/ProjectList.js
@@ -0,0 +1,34 @@
+import capstone from "../assets/capstone.JPG"
+import etchASketch from '../assets/etchASketch.JPG';
+import portfolio from '../assets/portfolio.JPG';
+import rps from '../assets/rps.JPG';
+import reactPortfolio from '../assets/reactPortfolio.JPG';
+import githubProfile from '../assets/githubProfile.JPG';
+
+
+export const ProjectList = [
+    {
+        name: 'Capstone Project',
+        image: capstone, 
+    },
+    {
+        name: 'React Personal Portfolio',
+        image: reactPortfolio,
+    },
+    {
+        name: 'Rock Paper Scissors Game',
+        image: rps,
+    },
+    {
+        name: 'Etch A Sketch',
+        image: etchASketch,
+    },
+    {
+        name: 'Porfolio',
+        image: portfolio,
+    },
+    {
+        name: 'Github Profile',
+        image: githubProfile,
+    }
+]
