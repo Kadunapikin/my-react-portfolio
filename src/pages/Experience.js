f7d091d96089b98b66ee937c86a2e82d780401ee
kadunapikin <ibakoshi27@gmail.com>
2023-08-10 11:50:47 +0100

    update Education section on Experience.js

--- src/pages/Experience.js
+++ src/pages/Experience.js
@@ -14,6 +14,23 @@ function Experience() {
           <h3 className='vertical-timline-element-title'>Nuhu Bamalli Polytechnic, Zaria KAduna State</h3>
           <p>Higher National Diploma in Agricultural Extension and Management</p>
         </VerticalTimelineElement>
+
+        <VerticalTimelineElement className='vertical-timeline-element--education' 
+        date='2021' 
+        iconStyle={{ background: '#3e497a', color: '#fff' }}
+        icon={<SchoolIcon />}>
+          <h3 className='vertical-timline-element-title'>Washington University, USA</h3>
+          <p>Project Management in Global Health (Remote)</p>
+        </VerticalTimelineElement>
+
+        <VerticalTimelineElement className='vertical-timeline-element--education' 
+        date='2022-2023' 
+        iconStyle={{ background: '#3e497a', color: '#fff' }}
+        icon={<SchoolIcon />}>
+          <h3 className='vertical-timline-element-title'>TOP Developers, Nigeria</h3>
+          <p>Front End Web Development (Remote)</p>
+        </VerticalTimelineElement>
+
       </VerticalTimeline>
     </div>
   )
