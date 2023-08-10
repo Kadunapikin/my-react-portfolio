808ce9a79408f99b13bdbddc72f797beb76afe1d
kadunapikin <ibakoshi27@gmail.com>
2023-08-10 11:10:35 +0100

    update Experience.js

--- src/pages/Experience.js
+++ src/pages/Experience.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import { VerticalTimeLine, VerticalTimelineElement } from 'react-vertical-timeline-component';
+import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
 import 'react-vertical-timeline-component/style.min.css';
 import SchoolIcon from '@mui/icons-material/School';
 
 function Experience() {
   return (
     <div className='experience'>
-      <VerticalTimeLine linecolor="#3e497a">
+      <VerticalTimeline linecolor="#3e497a">
         <VerticalTimelineElement className='vertical-timeline-element--education' 
         date='20011-2013' 
         iconStyle={{ background: '#3e497a', color: '#fff' }}
@@ -14,7 +14,7 @@ function Experience() {
           <h3 className='vertical-timline-element-title'>Nuhu Bamalli Polytechnic, Zaria KAduna State</h3>
           <p>Higher National Diploma in Agricultural Extension and Management</p>
         </VerticalTimelineElement>
-      </VerticalTimeLine>
+      </VerticalTimeline>
     </div>
   )
 }
