ef04486543d20e89cd920ac992849b2d23756fee
kadunapikin <ibakoshi27@gmail.com>
2023-08-10 10:41:33 +0100

    import react-vertical-timeline-component to Experience.js and add education section

--- src/pages/Experience.js
+++ src/pages/Experience.js
@@ -1,8 +1,21 @@
 import React from 'react'
+import { VerticalTimeLine, VerticalTimelineElement } from 'react-vertical-timeline-component';
+import 'react-vertical-timeline-component/style.min.css';
+import SchoolIcon from '@mui/icons-material/School';
 
 function Experience() {
   return (
-    <div>Experience</div>
+    <div className='experience'>
+      <VerticalTimeLine linecolor="#3e497a">
+        <VerticalTimelineElement className='vertical-timeline-element--education' 
+        date='20011-2013' 
+        iconStyle={{ background: '#3e497a', color: '#fff' }}
+        icon={<SchoolIcon />}>
+          <h3 className='vertical-timline-element-title'>Nuhu Bamalli Polytechnic, Zaria KAduna State</h3>
+          <p>Higher National Diploma in Agricultural Extension and Management</p>
+        </VerticalTimelineElement>
+      </VerticalTimeLine>
+    </div>
   )
 }
 
